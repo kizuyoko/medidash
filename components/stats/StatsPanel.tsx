7f664d31912db0db2aaad15c4c362675a94a6599
Yoko Kizu <kizuyoko@gmail.com>
2026-03-06 08:38:11 +0100

    Statspanel

--- components/stats/StatsPanel.tsx
+++ components/stats/StatsPanel.tsx
@@ -16,33 +16,60 @@ const StatsPanel = ({ patients } : Props) => {
       done: 0,
       cancelled: 0,
     };
+    const gender = {
+      male: 0,
+      female: 0,
+      unknown: 0,
+    };
+    const blood = {
+      'A+': 0, 'A-': 0, 'B+': 0, 'B-': 0, 'O+': 0, 'O-': 0, 'AB+': 0, 'AB-': 0,  Unknown: 0,
+    };
 
     patients.forEach(p => {
-      stats[p.status] += 1;
+      stats[p.status] ++;
+      gender[p.gender]++;
+      blood[p.bloodType]++;
     });
 
-    return stats;
+    return { stats, gender, blood};
   }, [patients]);
 
   const statsItems = [
-    { label: "Waiting", value: statusStats.waiting },
-    { label: "In consult", value: statusStats.in_consult },
-    { label: "Done", value: statusStats.done },
-    { label: "Cancelled", value: statusStats.cancelled },
+    { label: "Waiting", value: statusStats.stats.waiting, color: "bg-yellow-400" },
+    { label: "In consult", value: statusStats.stats.in_consult, color: "bg-purple-400"  },
+    { label: "Done", value: statusStats.stats.done, color: "bg-green-400"  },
+    { label: "Cancelled", value: statusStats.stats.cancelled, color: "bg-gray-400"  },
   ];
+
+  const bloodTypes = ["A+","A-","B+","B-","O+","O-","AB+","AB-"];
 
   return (
     <section className="w-full my-2 border border-slate-400 rounded-lg p-2 px-4 bg-slate-50 text-sm">
-      <ul>
-        <li className="md:flex gap-4"> Status
+      <ul className="flex flex-row md:flex-col gap-6 md:gap-2">
+        <li className="md:flex gap-4"><span className="inline-block w-20">Status</span>
          {
           statsItems.map(item => (
-            <p key={item.label}>{item.label}: {item.value} </p>
+            <p key={item.label}>
+              <span className={`inline-block mr-2 w-3 h-3 rounded-full ${item.color}`}></span>
+              {item.label}: {item.value} 
+            </p>
           ))
          }
         </li>
-        <li>Gender: Male: 10 Female 9</li>
-        <li>Blood type: Male: 10 Female 9</li>
+        <li className="md:flex gap-4">
+          <span className="inline-block w-20">Gender</span>
+          <p>Male: {statusStats.gender.male}</p>
+          <p>Female: {statusStats.gender.female}</p>
+          <p>Unknown: {statusStats.gender.unknown}</p>
+        </li>
+        <li className="md:flex gap-4">
+          <span className="inline-block w-20">Blood Type</span>
+          {bloodTypes.map(type => (
+            <p key={type}>
+              {type}: {statusStats.blood[type as keyof typeof statusStats.blood]}
+            </p>
+          ))}
+        </li>
       </ul>
     </section>
   );
